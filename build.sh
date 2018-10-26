32a710608b11bb6009beb4db613340d3bcc00442
Auston Stewart <auston@shedbuilt.net>
2018-10-25 21:01:08 -1000

    Updating to 2.58.1

--- build.sh
+++ build.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+# Patch
 # Apply BLFS log level patch
-patch -Np1 -i "${SHED_PKG_PATCH_DIR}/glib-2.54.3-skip_warnings-1.patch"
-# Apply BLFS meson patch
-patch -Np1 -i "${SHED_PKG_PATCH_DIR}/glib-2.54.3-meson_fixes-1.patch"
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/glib-2.58.1-skip_warnings-1.patch" &&
 # Apply our aarch64 meson host compilation patch
-patch -Np1 -i "${SHED_PKG_PATCH_DIR}/glib2-2.54.3_aarch64_meson_host.patch"
-mkdir build-glib && \
-cd build-glib && \
-meson --prefix=/usr \
-      -Dwith-pcre=system \
-      -Dwith-docs=no .. && \
-NINJAJOBS=$SHED_NUM_JOBS ninja && \
-DESTDIR="$SHED_FAKE_ROOT" ninja install && \
-chmod -v 755 "${SHED_FAKE_ROOT}"/usr/bin/{gdbus-codegen,glib-gettextize} && \
-mkdir -pv "${SHED_FAKE_ROOT}/usr/share/doc/glib-2.54.3" && \
-cp -r ../docs/reference/{NEWS,README,gio,glib,gobject} "${SHED_FAKE_ROOT}/usr/share/doc/glib-2.54.3"
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/glib2-2.54.3_aarch64_meson_host.patch" &&
 
+# Build and Install
+mkdir build-glib &&
+cd build-glib &&
+meson --prefix=/usr   \
+      -Dman=true      \
+      -Dselinux=false \
+      ..              &&
+NINJAJOBS=$SHED_NUM_JOBS ninja &&
+DESTDIR="$SHED_FAKE_ROOT" ninja install || exit 1
+
+# Install Documentation
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    mkdir -pv "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}" &&
+    cp -r ../docs/reference/{NEWS,gio,glib,gobject} "${SHED_FAKE_ROOT}${SHED_PKG_LOCAL_DOCDIR}"
+fi
